436d80fc4a3c6a49e184c849dcdfbd8f4b2d56fb
Dean Glueckler <glueckler@gmail.com>
2018-09-25 16:10:11 -0700

    added random site opener

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,6 +1,31 @@
-import $ from 'jquery';
-import RevealOnScroll from "./base/RevealOnScroll";
-import DavidLazyLoad from "./base/DavidLazyLoad";
+import $ from 'jquery'
+import RevealOnScroll from './base/RevealOnScroll'
+import DavidLazyLoad from './base/DavidLazyLoad'
 
-new RevealOnScroll($(".site-footer"),"100%");
-var davidLazyLoad = new DavidLazyLoad();
+new RevealOnScroll($('.site-footer'), '100%')
+var davidLazyLoad = new DavidLazyLoad()
+
+// let's get dirty
+var randoSites = [
+  'http://deanglueckler.com/hi/',
+  'https://hisimaginationn.com/',
+  'http://deanglueckler.com/pp/',
+  'http://funqmedia.com/ifbrv2/',
+  'http://underscormusic.com/',
+  'http://deanglueckler.com/pp/weather/index.html',
+  'http://deanglueckler.com/pp/quotes/index.html',
+  'http://deanglueckler.com/pp/wikipedia/index.html',
+  'http://deanglueckler.com/pp/twitch/index.html',
+]
+var randomLink = document.querySelector('#random-link')
+
+function openInNewTab(url) {
+  var win = window.open(url, '_blank')
+  win.focus()
+}
+
+randomLink.addEventListener('click', () => {
+  const sitesLen = randoSites.length
+  var ranIndex = (sitesLen + Math.floor(Math.random() * sitesLen)) % sitesLen
+  openInNewTab(randoSites[ranIndex])
+})
--- app/temp/scripts/App.js
+++ app/temp/scripts/App.js
@@ -10344,8 +10344,23 @@ var _DavidLazyLoad2 = _interopRequireDefault(_DavidLazyLoad);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-new _RevealOnScroll2.default((0, _jquery2.default)(".site-footer"), "100%");
+new _RevealOnScroll2.default((0, _jquery2.default)('.site-footer'), '100%');
 var davidLazyLoad = new _DavidLazyLoad2.default();
+
+// let's get dirty
+var randoSites = ['http://deanglueckler.com/hi/', 'https://hisimaginationn.com/', 'http://deanglueckler.com/pp/', 'http://funqmedia.com/ifbrv2/', 'http://underscormusic.com/', 'http://deanglueckler.com/pp/weather/index.html', 'http://deanglueckler.com/pp/quotes/index.html', 'http://deanglueckler.com/pp/wikipedia/index.html', 'http://deanglueckler.com/pp/twitch/index.html'];
+var randomLink = document.querySelector('#random-link');
+
+function openInNewTab(url) {
+  var win = window.open(url, '_blank');
+  win.focus();
+}
+
+randomLink.addEventListener('click', function () {
+  var sitesLen = randoSites.length;
+  var ranIndex = (sitesLen + Math.floor(Math.random() * sitesLen)) % sitesLen;
+  openInNewTab(randoSites[ranIndex]);
+});
 
 /***/ }),
 /* 2 */
